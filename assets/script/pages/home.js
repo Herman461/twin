3d54e11e626244a126047c5b742a50c6e28b6af4
Herman461 <zhukovskiyherman@gmail.com>
2022-08-27 21:53:33 +0300

    fix mainslider

--- assets/script/pages/home.js
+++ assets/script/pages/home.js
@@ -2,7 +2,11 @@ const introSlider = new Swiper('.intro-slider', {
     speed: 1000,
     loop: true,
     spaceBetween: 40,
+    autoplay: {
+        delay: 2000,
+    },
     pagination: {
         el: ".intro-pagination",
+        clickable: true,
     },
 })
