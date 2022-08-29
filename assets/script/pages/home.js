df68d27df3a2c83b5b5b53041dcdd0d5f3269f5d
Herman461 <zhukovskiyherman@gmail.com>
2022-08-29 12:43:58 +0300

    fix homepage

--- assets/script/pages/home.js
+++ assets/script/pages/home.js
@@ -2,9 +2,10 @@ const introSlider = new Swiper('.intro-slider', {
     speed: 1000,
     loop: true,
     spaceBetween: 40,
-    // autoplay: {
-    //     delay: 2000,
-    // },
+    autoplay: {
+        delay: 2000,
+    },
+    autoHeight: true,
     pagination: {
         el: ".intro-pagination",
         clickable: true,
